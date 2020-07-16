5cdc8807f1c84662983cc838b99b06039ac13e67
Jeffrey Hagenstein <jdhagenstein@gmail.com>
2020-07-16 20:57:35 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name='postbode-python',
+    packages=['postbode-python'],
+    version='0.1',
+    license='MIT',
+    description='Postbode.nu api wrapper for python',
+    author='Jeffrey Hagenstein',
+    author_email='jeffrey@hef-it.nl',
+    # Provide either the link to your github or to your website
+    url='https://github.com/user/reponame',
+    # I explain this later on
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords=['postnode', 'api', 'mail', 'postal'],
+    install_requires=[
+        # TODO
+    ],
+    classifiers=[
+        # TODO
+    ],
+)
